b4cfc3c2ce9da8d4b3952154de69615b174df2d5
Eduardo Moroni <eduardomoroni@gmail.com>
2017-10-11 20:39:37 -0300

    fix: retorna ExpenseClientResponse em ExpenseClient

--- src/main/kotlin/br/com/bumblebee/congressman/client/ExpenseClient.kt
+++ src/main/kotlin/br/com/bumblebee/congressman/client/ExpenseClient.kt
@@ -1,9 +1,8 @@
 package br.com.bumblebee.congressman.client
 
 import br.com.bumblebee.configuration.feign.FeignConfiguration
-import br.com.bumblebee.congressman.client.model.CongressmanClientResponse
+import br.com.bumblebee.congressman.client.model.ExpenseClientResponse
 import org.springframework.cloud.netflix.feign.FeignClient
-import org.springframework.http.MediaType
 import org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -11,13 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 
 @FeignClient(name = "expense", url = "\${camara.api}", configuration = arrayOf(FeignConfiguration::class))
-@RequestMapping(produces = arrayOf(APPLICATION_JSON_UTF8_VALUE)))
+@RequestMapping(produces = arrayOf(APPLICATION_JSON_UTF8_VALUE))
 interface ExpenseClient {
 
     @GetMapping("deputados/{congressmanId}/despesas")
     fun getCongressmanExpenses(
-        @PathVariable(name = "id") id: Int,
+        @PathVariable(name = "congressmanId") id: Int,
         @RequestParam(name = "pagina") page: Int = 1
-    ): CongressmanClientResponse
+    ): ExpenseClientResponse
 
 }
