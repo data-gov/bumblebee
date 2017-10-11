360e72a148706fe1f0646cd0d29c1153e9fd4921
Eduardo Moroni <eduardomoroni@gmail.com>
2017-10-11 09:41:28 -0300

    ci: adiciona teste de integracao

--- src/test/unit/kotlin/br/com/bumblebee/congressman/mapper/CongressmanMapperTest.kt
+++ src/test/unit/kotlin/br/com/bumblebee/congressman/mapper/CongressmanMapperTest.kt
@@ -1,21 +1,32 @@
 package br.com.bumblebee.congressman.mapper
 
-import br.com.bumblebee.congressman.client.model.CongressmanClientModel
-import br.com.bumblebee.congressman.client.model.CongressmanClientResponse
-import br.com.bumblebee.congressman.controller.model.CongressmanResponse
+import br.com.bumblebee.congressman.repository.ExpenseRepository
+import br.com.bumblebee.congressman.repository.model.Expense
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
-internal class CongressmanMapperTest {
+@ExtendWith(SpringExtension::class)
+@SpringBootTest
+internal class ExpenseRepositoryTest {
+
+    @Autowired
+    private lateinit var repository: ExpenseRepository
+
+    @BeforeEach
+    fun setUp() = repository.deleteAll()
 
     @Test
-    fun shouldTransformCongressmanClientResponseIntoCongressmanResponse() {
-        val actual = toCongressmanResponse(CongressmanClientResponse(listOf(aCongressman())))
-        val expected = listOf(CongressmanResponse(1, "Congressman Name", "PPP", "UF", "Photo URI"))
+    @DisplayName("Should save a congressman expense into mongodb")
+    fun assertThatExpenseIseBeingSaved() {
+        repository.save(Expense("SUPER_COOL_ID"))
+        val allExpenses = repository.findAll()
 
-        assertEquals(actual, expected)
+        Assertions.assertEquals(1, allExpenses.size)
     }
-
-    private fun aCongressman() =
-        CongressmanClientModel(1, "URI", "Congressman Name", "PPP", "PPP URI", "UF", 1, "Photo URI")
 }
