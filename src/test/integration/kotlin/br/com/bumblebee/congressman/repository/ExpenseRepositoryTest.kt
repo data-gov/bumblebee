6970809a63f95dabaa3815b5a091de1c746f906b
Vinicius Costa <viniciusrdacosta@gmail.com>
2017-10-12 11:25:06 -0300

    refactor: Troca versao do junit para 4.12 e ajustas as configuracoes (#8)
    * refactor: Troca versao do junit para 4.12 e ajustas as configuracoes
    
    * chore: Configura relatorios de qualidade de código

--- src/test/integration/kotlin/br/com/bumblebee/congressman/repository/ExpenseRepositoryTest.kt
+++ src/test/integration/kotlin/br/com/bumblebee/congressman/repository/ExpenseRepositoryTest.kt
@@ -1,28 +1,25 @@
 package br.com.bumblebee.congressman.repository
 
 import br.com.bumblebee.congressman.repository.model.Expense
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.context.junit4.SpringRunner
+import kotlin.test.assertEquals
 
-
-@ExtendWith(SpringExtension::class)
 @SpringBootTest
+@RunWith(SpringRunner::class)
 internal class ExpenseRepositoryTest {
 
     @Autowired
     private lateinit var repository: ExpenseRepository
 
-    @BeforeEach
+    @Before
     fun setUp() = repository.deleteAll()
 
     @Test
-    @DisplayName("Should save a congressman expense into mongodb")
     fun assertThatExpenseIseBeingSaved() {
         repository.save(Expense("SUPER_COOL_ID"))
         val allExpenses = repository.findAll()
--- src/test/unit/kotlin/br/com/bumblebee/congressman/controller/CongressmanControllerTest.kt
+++ src/test/unit/kotlin/br/com/bumblebee/congressman/controller/CongressmanControllerTest.kt
@@ -5,20 +5,19 @@ import br.com.bumblebee.congressman.client.model.CongressmanClientModel
 import br.com.bumblebee.congressman.client.model.CongressmanClientResponse
 import com.nhaarman.mockito_kotlin.whenever
 import org.hamcrest.Matchers.hasSize
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.Test
+import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
-import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-
-@ExtendWith(SpringExtension::class)
+@RunWith(SpringRunner::class)
 @WebMvcTest(CongressmanController::class)
 internal class CongressmanControllerTest {
 
