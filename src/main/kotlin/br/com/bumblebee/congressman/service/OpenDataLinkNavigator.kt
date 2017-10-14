185f2898eb66be58402203cf0e29083b595d79f6
Vinicius Costa <viniciusrdacosta@gmail.com>
2017-10-14 11:42:23 -0300

    refactor: Remove modificador Open da classe OpenDataLinkNavigator

--- src/main/kotlin/br/com/bumblebee/congressman/service/OpenDataLinkNavigator.kt
+++ src/main/kotlin/br/com/bumblebee/congressman/service/OpenDataLinkNavigator.kt
@@ -2,7 +2,7 @@ package br.com.bumblebee.congressman.service
 
 import br.com.bumblebee.congressman.client.model.OpenDataResponse
 
-open class OpenDataLinkNavigator<T> {
+class OpenDataLinkNavigator<T> {
     fun navigate(url: String): OpenDataResponse<T> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
