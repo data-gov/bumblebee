93c2ca2a44fdb95d37eb3c39f6d94e7e512e8825
Vinicius Costa <viniciusrdacosta@gmail.com>
2017-10-15 14:39:31 -0200

    feat(Configuracao Client): Adiciona level de logs baseado no arquivo de configuracao

--- src/main/kotlin/br/com/bumblebee/configuration/feign/FeignConfiguration.kt
+++ src/main/kotlin/br/com/bumblebee/configuration/feign/FeignConfiguration.kt
@@ -1,14 +1,18 @@
 package br.com.bumblebee.configuration.feign
 
 import feign.Logger
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class FeignConfiguration {
 
+    @Value("\${logging.feign.type}")
+    lateinit var loggerType: String
+
     @Bean
     fun logger(): Logger.Level {
-        return Logger.Level.FULL
+        return Logger.Level.valueOf(loggerType)
     }
 }
