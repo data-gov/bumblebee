a5d36ffa59222ca6f52f6ad0937f47bf46efe16e
Vinicius Costa <viniciusrdacosta@gmail.com>
2017-10-19 11:16:20 -0200

    refactor: Renomeia nome da collection de congressman

--- src/main/kotlin/br/com/bumblebee/congressman/repository/model/Congressman.kt
+++ src/main/kotlin/br/com/bumblebee/congressman/repository/model/Congressman.kt
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import java.util.Date
 
-@Document(collection = "congressman")
+@Document(collection = "congressmen")
 data class Congressman(
     @Id val id: Int,
     val name: String,
