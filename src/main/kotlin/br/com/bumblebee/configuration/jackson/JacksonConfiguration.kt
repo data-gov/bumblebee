ca065e5ec0fb9ce6c3be8c893ad9bf9b50346550
Vinicius Costa <viniciusrdacosta@gmail.com>
2017-12-22 11:07:25 -0200

    feat: Remove api de eleicoes

--- src/main/kotlin/br/com/bumblebee/configuration/jackson/JacksonConfiguration.kt
+++ src/main/kotlin/br/com/bumblebee/configuration/jackson/JacksonConfiguration.kt
@@ -2,7 +2,6 @@ package br.com.bumblebee.configuration.jackson
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include.NON_EMPTY
 import com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES
-import com.fasterxml.jackson.dataformat.csv.CsvMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -17,7 +16,4 @@ class JacksonConfiguration {
             .modulesToInstall(KotlinModule())
             .serializationInclusion(NON_EMPTY)
             .featuresToDisable(FAIL_ON_UNKNOWN_PROPERTIES)
-
-    @Bean
-    fun csvMapper() = CsvMapper()
 }
